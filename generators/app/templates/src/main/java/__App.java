7cd827790608c7fd8a6c59019834a4512a9c50e7
Emerson Cantalice <emersoncantalicee@gmail.com>
2017-11-29 01:08:32 -0200

    configuracao de cache adicionada

--- generators/app/templates/src/main/java/__App.java
+++ generators/app/templates/src/main/java/__App.java
@@ -2,17 +2,39 @@ package <%= packageName %>;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.spring.cache.HazelcastCacheManager;
+
 @SpringBootApplication
+@EnableCaching
 public class <%= projectName %>App {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(<%= projectName %>App.class, args);
+    }
+
+     @Bean
+    public HazelcastInstance hazelcastInstance() {
+        ClientConfig config = new ClientConfig();
+        config.getGroupConfig().setName("dev").setPassword("dev-pass");
+        config.getNetworkConfig().addAddress("192.168.99.100");
+        config.setInstanceName("cache-1");
+        HazelcastInstance instance = HazelcastClient.newHazelcastClient(config);
+        return instance;
+    }
+ 
+    @Bean
+    public CacheManager cacheManager() {
+        return new HazelcastCacheManager(hazelcastInstance());
     }
 
     @Bean
