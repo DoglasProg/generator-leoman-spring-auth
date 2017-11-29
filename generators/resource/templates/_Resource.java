5cd83e473e96ecfb9e1ab1adcd009078e316df5b
Emerson Cantalice <emersoncantalicee@gmail.com>
2017-11-29 17:24:22 -0200

    Correção geraçao de cruds

--- generators/resource/templates/_Resource.java
+++ generators/resource/templates/_Resource.java
@@ -1,23 +1,19 @@
-package <%= packageName %>;
+package <%= packageName %>.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
+import org.springframework.data.annotation.Id;
 
 public class <%= resourceName %> extends EntityGeneric {
 
-    @Id
-    private Long id;
+      @Id
+    private String id;
 
     private String name;
     
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
--- generators/resource/templates/__Resource.java
+++ generators/resource/templates/__Resource.java
@@ -1,4 +1,4 @@
-package <%= packageName %>;
+package <%= packageName %>.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
